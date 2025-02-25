596c50691129cf38a20a2d6c9dcc985f5b7c7de2
dovahlore <mayuqing11111@qq.com>
2025-02-25 18:54:31 +0800

    update

--- edge.py
+++ edge.py
@@ -2,11 +2,15 @@
 from selenium.webdriver import Edge
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import base64
+import random
 #用于选择登录端口
 import time
 from selenium.webdriver.edge.options import Options
 #造浏览器配置对象
+#通过百度页面，搜索烤鸭
+print("connecting")
 Edge_op = Options()
 #配置浏览器
 #"127.0.0.1:9222"其中，9222是浏览器的运行端口
@@ -15,19 +19,33 @@ Edge_op.add_experimental_option("debuggerAddress","127.0.0.1:9222")
 browser = Edge(options=Edge_op)
 #测试环节
 #通过百度页面，搜索烤鸭
+print("linked to driver")
+url=input("url input:")
+browser.get(url)
 
-while input()!='ok':
+print("ready to start？输入“y”开始")
+while input()!='y':
     time.sleep(1)
 
+
+
 i=0
+print("url:",browser.current_url)
+next_page = browser.find_element(By.CLASS_NAME, "swiper-button-next")
 while True:
     try:
         canvas = browser.find_element(By.ID, "page%d"%i)
-
+        # 找到 canvas 元素
         # 你可以对找到的 canvas 元素进行进一步操作，例如获取其大小
         width = canvas.get_attribute("width")
         height = canvas.get_attribute("height")
         print(f"canvas 元素的大小为：{width}x{height}")
+        while width == "300" :
+            width = canvas.get_attribute("width")
+            height = canvas.get_attribute("height")
+            time.sleep(5)
+
+
         i+=1
 
         # 使用 JavaScript 获取 canvas 的 data URL
@@ -40,7 +58,15 @@ while True:
         # 将图片保存为文件
         with open("pic/page%d.jpg"%i, "wb") as image_file:
             image_file.write(image_bytes)
+            print(f"已保存第{i}页图片")
+        time.sleep(random.randint(4,8))
+        actions = ActionChains(browser)
+
+        actions.click(next_page)
+        actions.perform()
+        time.sleep(random.randint(8,10))# 300 表示移动的像素
     except Exception as e:
+        print(e)
         break
 
 #swiper-lazy swiper-lazy-loaded
